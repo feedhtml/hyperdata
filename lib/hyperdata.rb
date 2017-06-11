9efca1afeae23e6e5740eeae1b3ad73cf1384b79
Gerald Bauer <gerald.bauer@gmail.com>
2017-06-11 12:52:42 +0200

    up

--- lib/hyperdata.rb
+++ lib/hyperdata.rb
@@ -3,14 +3,27 @@
 
 # core and stdlibs
 
+require 'json'
+require 'date'
+require 'time'
+require 'pp'
+
 
 # 3rd party gems/libs
+require 'logutils'
 
+require 'nokogiri'
 
 
 # our own code
-
 require 'hyperdata/version'  # let it always go first
+
+require 'hyperdata/feed'
+require 'hyperdata/item'
+
+require 'hyperdata/builder/article'
+require 'hyperdata/parser'
+
 
 
 # say hello
